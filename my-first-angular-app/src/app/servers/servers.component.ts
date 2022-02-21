8191a6f605ba0a8a89c92370743ef2390a79abfb
Ryan Braun <ryan.braun@ahead.com>
2022-02-21 15:21:30 -0800

    binding

--- my-first-angular-app/src/app/servers/servers.component.ts
+++ my-first-angular-app/src/app/servers/servers.component.ts
@@ -2,14 +2,29 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  // template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus ="No Server was created!";
+  serverName="";
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created!";
+  }
+  
+  onUpdateServerName(event: any) {
+   this.serverName = (<HTMLInputElement>event.target).value;
+
+  }
 }
